59235340f72f6011a04848950ddfd3b8229eb827
VinnTech <kevinginting605@gmail.com>
2023-12-23 21:22:48 +0700

    test deploy

--- src/utils/api-endpoint.jsx
+++ src/utils/api-endpoint.jsx
@@ -2,7 +2,8 @@ export const API_ENDPOINT = {
   REGISTER_PAGE: "api/auth/register",
   FORGOT_PASSWORD: "api/auth/forgot-password",
   RESET_PASSWORD: "api/auth/reset-password",
+  AUTHENTICATED: "api/auth/authenticated",
   CLASSES: "api/classes",
   PAYMENTS: "api/payments",
-  UPDATE_PAYMENTS: "api/payments/paid/"
+  UPDATE_PAYMENTS: "api/payments/paid"
 };
